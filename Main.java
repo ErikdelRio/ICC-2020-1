daecbfd46048afb8d5f9bffff3e1492f57e1c707
ErikdelRio <erik.delrio@ciencias.unam.mx>
2019-08-13 06:06:43 -0500

    Practica de laboratorio 1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+	System.out.println(
+	    "openjdk 11.0.4 2019-07-16\n"+
+	    "OpenJDK Runtime Environment (build 11.0.4+11)\n"+
+	    "OpenJDK 64-Bit Server VM (build 11.0.4+11, mixed mode");
+	    
+    }
+}
