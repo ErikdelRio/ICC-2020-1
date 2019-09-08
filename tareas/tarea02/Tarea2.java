4d897de3ce543382b83de3624a12ba52d1e4298b
ErikdelRio <erik.delrio@ciencias.unam.mx>
2019-09-08 16:37:13 -0500

    Tarea 2 de fechas listo

--- tareas/tarea02/Tarea2.java
+++ tareas/tarea02/Tarea2.java
@@ -1,16 +1,17 @@
 import java.util.Date;
 import java.util.Scanner;
 
-/** Programa para 
+/** Programa para calcular la edad de una persona en años, meses y días
  * @author del Rio Pulido Erik Federico
- * @date 
+ * @date 8 de septiembre de 2019 
  */
 public class Tarea2 {
     public static void main(String[] pps) {
-                      // Declaracion de variables
+            // Declaracion de variables
 	Scanner in = new Scanner(System.in);
 	int dia, mes, anio;
-	long edad;
+	int edad, meses, dias, mesesActuales, diaCumple;
+	long tiempo;
 	Date hoy = new Date(), cumple;
 	    // Solicitar la fecha de nacimiento
 	System.out.println("Dame tu fecha de nacimiento");
@@ -25,54 +26,57 @@ public class Tarea2 {
 	
 	    // Adaptar la fecha normal al formato de Date
 	cumple = new Date(anio-1900,mes-1,dia);
-
 	if (cumple.getTime() > hoy.getTime()){
-	    System.out.println("No se permiten viajeros en el tiempo!");
+	    System.out.println("¡No se permiten viajeros en el tiempo!");
+	} else if(cumple.getYear()<0){
+	    System.out.println("Tienes demasiados años");
 	} else {
 	    //Mostrar las fechas
-	    System.out.println("\n\nCumple: "+cumple.toString());
-	    System.out.println("Hoy: "+hoy.toString());
+	    System.out.println("\nTu cumple es: "+cumple.getDate()+"/"+(cumple.getMonth()+1)+"/"+(cumple.getYear()+1900) + "/");
+	    System.out.println("Hoy es: "+hoy.getDate()+"/"+(hoy.getMonth()+1)+"/"+(hoy.getYear()+1900) + "/");
 
 	    //Calcular la edad de una persona.
 
 	    //Años
-	    edad = hoy.getYear() - cumple.getYear();
-
-	    if(hoy.getMonth()<cumple.getMonth() && hoy.getDate()<cumple.getDate()){
-		edad -= 1;
+	    edad = 0;
+	    tiempo = cumple.getTime();
+	    cumple.setYear(cumple.getYear()+1);
+	    while(cumple.getTime() < hoy.getTime()){
+		tiempo = cumple.getTime();
+		cumple.setYear(cumple.getYear()+1);
+		edad += 1;
 	    }
-	    System.out.println("Hoy: "+hoy.getDate());
-	    System.out.println("Cumple: "+cumple.getDate());
+	    cumple.setTime(tiempo);
 
 	    // Meses
-	    int meses = cumple.getMonth()-hoy.getMonth();
-	    System.out.println("Cumple meses="+cumple.getMonth()+" Hoy meses="+hoy.getMonth());
-	    if(meses <= 0){
-		meses *= -1; // Convertir a positivo
-	    } else {
-		meses = 12 - meses;
-	    }
+	    meses = 0;
+	    mesesActuales = cumple.getMonth();
+	    diaCumple = cumple.getDate();
+	    do{
+		tiempo = cumple.getTime();
+		cumple.setMonth(cumple.getMonth()+1);
+		cumple.setDate(diaCumple);
+		while(cumple.getMonth()>(mesesActuales+1)%12){ // Si 31 pasa a 30-28
+		    cumple.setDate(cumple.getDate()-1);
+		}
+		meses += 1;
+		mesesActuales += 1; 
+	    }while(cumple.getTime() < hoy.getTime());
+	    cumple.setTime(tiempo);
+	    meses -= 1;
 
 	    // dias
-	    int dias = cumple.getDate() - hoy.getDate();
-	    if(dias <= 0){
-		dias *= -1;
-	    } else {
-		int cont = 0, mesActual = cumple.getMonth();
-		cumple.setMonth(cumple.getMonth()-1);
-		while(cumple.getMonth() != mesActual){
-		    cumple.setDate(cumple.getDate()+1);
-		    cont += 1;
-		}
-		dias = cont + hoy.getDate();
+	    dias = 0;
+	    cumple.setDate(cumple.getDate()+1);
+	    while(cumple.getTime() <= hoy.getTime()){
+		tiempo = cumple.getTime();
+		cumple.setDate(cumple.getDate()+1);
+		dias += 1;
 	    }
+	    cumple.setTime(tiempo);
+
 	    // Mostrar resultados.
 	    System.out.println("Tu edad es: " + edad + " años, "+ meses+" meses, "+ dias+" días");
 	}
-	
-            // Mostrar las fechas
-	    //Calcular la edad de una persona. 
-
-	    // Mostrar resultados.
     }
 }
