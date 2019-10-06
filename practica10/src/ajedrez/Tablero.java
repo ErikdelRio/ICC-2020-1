8d22f000b49fff67a67ffaf4d03f9ad99b3993db
ErikdelRio <erik.delrio@ciencias.unam.mx>
2019-10-06 11:43:16 -0500

    Practica 10, las ventanas son relativas a la pantalla

--- practica10/src/ajedrez/Tablero.java
+++ practica10/src/ajedrez/Tablero.java
@@ -2,18 +2,20 @@ package ajedrez;
 
 import processing.core.PApplet;
 // javac -classpath lib/core.jar -d build src/paquete/A.java (direccion)
-// java --classpath build:lib/core
+// java --classpath build:lib/core paquete.Clase
 // fill(int, int, int) | fill(int)
 // rect(x,y,w,h)
 
 public class Tablero extends PApplet {
+    int ancho;
     public static void main(String[] args){
 	PApplet.main("ajedrez.Tablero");
     }
 
     @Override
     public void settings(){
-	size(400, 400);
+	ancho = displayHeight * 2 / 3;
+	size(ancho, ancho);
     }
     
     @Override
@@ -23,7 +25,7 @@ public class Tablero extends PApplet {
     @Override
     public void draw(){
 	int negro = 0xFF666666, blanco = 0xFFFFFFFF;
-	int tam = 50;
+	int tam = ancho/8;
 	for (int i=0; i<8; i++){
 	    for(int j=0; j<8; j++){
 		int color =  ((i+j)%2 == 0)? blanco : negro;
